bd57dc87b7f43f7f86bd5dafe3e2511384bc5afb
LubovSchmit <81571492+LubovSchmit@users.noreply.github.com>
2024-04-16 16:15:50 +0200

    commit

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import './App.css';
+import { HW1 } from './HW/1/HW1';
+import { HW2 } from './HW/2/HW2';
+import { HW3 } from './HW/3/HW3';
+import { HW4 } from './HW/4/HW4';
+
+function App() {
+  return (
+      <div className="App">
+          <HW1/>
+          <br/>
+         ________________________________________________
+          <br/>
+          <HW2/>
+          <br/>
+          ________________________________________________
+          <br/>
+          <HW3/>
+          <br/>
+          ________________________________________________
+
+          <HW4/>
+      </div>
+  );
+}
+
+export default App;
